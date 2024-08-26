a2c2345e8641127152de8dd9f11009ee8eec480a
ABHINAV H K <83592120+abhinavhk17@users.noreply.github.com>
2024-08-27 00:09:45 +0530

    Initial commit

---
+++ src/main/java/com/Ecommerce/Ecommerce/Controller/Controller.java
@@ -0,0 +1,17 @@
+package com.Ecommerce.Ecommerce.Controller;
+import com.Ecommerce.Ecommerce.Entity.Ecommerce;
+import com.Ecommerce.Ecommerce.Service.Ecommerce.MobileService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/mobile")
+
+public class Controller {
+    @Autowired
+    private
+}
